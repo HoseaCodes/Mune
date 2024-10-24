a295edb6b5f60442e07ab5e0a4b805cfd6bbdfbc
Riviere Fougy <rivierefougy@gmail.com>
2024-10-23 20:17:51 -0400

    feat: create submit card on submit click

--- src/components/contact/contact-form/ContactForm.tsx
+++ src/components/contact/contact-form/ContactForm.tsx
@@ -38,7 +38,7 @@ const validationSchema = yup.object().shape({
     .max(250, {
       field: 'message',
       message: 'Message must not exceed 250 characters',
-    })
+    }),
 });
 
 const ContactForm: React.FC = () => {
@@ -58,6 +58,8 @@ const ContactForm: React.FC = () => {
   }>({});
   const [buttonDisabled, setButtonDisabled] =
     useState<boolean>(true);
+  const [displaySubmitCard, setDisplaySubmitCard] =
+    useState<boolean>(false);
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -87,6 +89,7 @@ const ContactForm: React.FC = () => {
       console.log('success');
       setFormData({ email: '', name: '', message: '' });
       setFormErrors({});
+      setDisplaySubmitCard(true);
     } catch (err) {
       console.log('failure');
 
@@ -119,40 +122,55 @@ const ContactForm: React.FC = () => {
   );
 
   return (
-    <form
-      noValidate
-      onSubmit={handleSubmit}
-      className="space-y-6 bg-[#1AAE33] px-4 py-8 rounded-[48px] w-full"
-    >
-      <TextField
-        type="name"
-        name="name"
-        val={formData.name}
-        validationError={formErrors.name}
-        placeholder="Name"
-        handleChange={handleChange}
-      />
-      <TextField
-        type="email"
-        name="email"
-        val={formData.email}
-        validationError={formErrors.email}
-        placeholder="Email"
-        handleChange={handleChange}
-      />
-      <MessageTextArea
-        val={formData.message}
-        validationError={formErrors.message}
-        handleChange={handleChange}
-      />
-      <button
-        disabled={buttonDisabled}
-        type="submit"
-        className={`${buttonDisabled && 'opacity-50'} w-content flex justify-center py-3 px-6 border-2 border-[#CEE0D0] rounded-xl shadow-sm text-base bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-400 text-black font-semibold`}
+    <div className="relative">
+      {displaySubmitCard && (
+        <div className="absolute z-10 bg-[#1AAE33] h-full w-full px-4 py-8 rounded-[48px]">
+          <p className="text-white">
+            Thanks for Contact Us
+          </p>
+          <button
+            className="text-white underline"
+            onClick={() => setDisplaySubmitCard(false)}
+          >
+            Contact Us
+          </button>
+        </div>
+      )}
+      <form
+        noValidate
+        onSubmit={handleSubmit}
+        className="space-y-6 bg-[#1AAE33] px-4 py-8 rounded-[48px] w-full"
       >
-        Submit
-      </button>
-    </form>
+        <TextField
+          type="name"
+          name="name"
+          val={formData.name}
+          validationError={formErrors.name}
+          placeholder="Name"
+          handleChange={handleChange}
+        />
+        <TextField
+          type="email"
+          name="email"
+          val={formData.email}
+          validationError={formErrors.email}
+          placeholder="Email"
+          handleChange={handleChange}
+        />
+        <MessageTextArea
+          val={formData.message}
+          validationError={formErrors.message}
+          handleChange={handleChange}
+        />
+        <button
+          disabled={buttonDisabled}
+          type="submit"
+          className={`${buttonDisabled && 'opacity-50'} w-content flex justify-center py-3 px-6 border-2 border-[#CEE0D0] rounded-xl shadow-sm text-base bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-400 text-black font-semibold`}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
