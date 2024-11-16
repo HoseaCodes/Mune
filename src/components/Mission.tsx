95267602dfcba0a1683aea707d5c0a4b208d5ac3
Riviere Fougy <rivierefougy@gmail.com>
2024-11-15 21:35:24 -0500

    feat: create ProgramBenefitsCard, apply to Mission, add MissionCard images

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import MissionCard from './about/MissionCard';
+import ProgramBenefitsCard from './about/ProgramBenefitsCard';
 import familySelfie from '../assets/pages/about/family-selfie.webp';
-import MissionCard from './home/MissionCard';
+import empowermentImage from '../assets/pages/about/empowerment.webp';
+import donationCommitmentImage from '../assets/pages/about/donation-commitment.webp';
+import whyWeDoThisImage from '../assets/pages/about/why-we-do-this.webp';
+
+import fundingIcon from '../assets/pages/about/benefits-funding-icon.svg';
+import mentorshipIcon from '../assets/pages/about/benefits-mentorship-icon.svg';
+import scholoarshipsIcon from '../assets/pages/about/benefits-scholarships-icon.svg';
 
 const MissionSection: React.FC = () => {
   return (
-    <div className="flex flex-col gap-4 max-w-[1184px] mx-6 laptop:mx-12">
-      <div className="relative py-10 laptop:py-20 px-6 laptop:px-8 flex flex-col items-center border-2 laptop:border-none rounded-[32px] border-[#D9E9E2] laptop:bg-none">
+    <div className="flex flex-col gap-20 max-w-[1184px] mx-6 laptop:mx-12">
+      <div className="relative pt-10 laptop:pt-20 px-6 laptop:px-8 flex flex-col items-center border-2 laptop:border-none rounded-[32px] border-[#D9E9E2] laptop:bg-none">
         <div className="laptop:absolute laptop:top-0 laptop:left-0 flex flex-col gap-2 items-center laptop:items-start laptop:bg-[#F2FDF9F2] laptop:border-2 border-[#D9E9E2] rounded-[48px] max-w-fit py-8 px-6">
           <h2 className="font-bold text-4xl text-center text-[#182E26]">
             How we give back to the community:
@@ -23,19 +31,38 @@ const MissionSection: React.FC = () => {
         <MissionCard
           title="Empowering Black Entrepreneurs to Thrive"
           copy="The Mun-e Uplift Program is a financial and mentorship initiative designed to empower Black small business owners and aspiring entrepreneurs."
-          imageSrc={familySelfie}
+          imageSrc={empowermentImage}
         />
         <MissionCard
           title="Donation Commitment"
           copy="Through this program, Mun-e donates 10% of its profits to award grants and scholarships to deserving individuals."
-          imageSrc={familySelfie}
+          imageSrc={donationCommitmentImage}
           reverseOrder
         />
         <MissionCard
           title="Why We Do This"
-          copy="Mun-e recognizes the unique challenges faced by Black entrepreneurs in securing funding and guidance. The Mun-e Uplift Program bridges this gap by providing critical financial resources and mentorship opportunities to help  Black-owned businesses flourish."
-          imageSrc={familySelfie}
+          copy="Mun-e recognizes the unique challenges faced by Black entrepreneurs in securing funding and guidance. The Mun-e Uplift Program bridges this gap by providing critical financial resources and mentorship opportunities to help Black-owned businesses flourish."
+          imageSrc={whyWeDoThisImage}
           displayButtons
+        />
+      </div>
+      <div className="flex flex-col laptop:flex-row gap-4">
+        <div className="flex flex-col tablet:flex-row gap-4">
+          <ProgramBenefitsCard
+            title="Grant Funding"
+            copy="Uplift grants provide financial support to help cover essential business needs such as equipment, marketing, and inventory"
+            imageSrc={fundingIcon}
+          />
+          <ProgramBenefitsCard
+            title="Mentorship"
+            copy="Grantees will be paired with experienced business professionals for personalized mentorship and guidance."
+            imageSrc={mentorshipIcon}
+          />
+        </div>
+        <ProgramBenefitsCard
+          title="Scholarship Opportunities"
+          copy="Scholarships are available to support educational programs relevant to business growth and entrepreneurship."
+          imageSrc={scholoarshipsIcon}
         />
       </div>
     </div>
---
+++ src/components/about/ProgramBenefitsCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProgramBenefitsCard: React.FC<{
+  title: string;
+  copy: string;
+  imageSrc: string;
+}> = ({ title, copy, imageSrc }) => (
+  <div className="flex-2 flex flex-col gap-2 tablet:border rounded-3xl py-4 px-3">
+    <img src={imageSrc} alt="" className="w-6" />
+    <div className="text-[15px] font-semibold">{title}</div>
+    <div className="text-[13px] font-normal me-12">
+      {copy}
+    </div>
+  </div>
+);
+
+export default ProgramBenefitsCard;
