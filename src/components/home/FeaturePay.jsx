15e9a70cfb9c0dd3630cab7fb62313756494b79d
Riviere Fougy <rivierefougy@gmail.com>
2024-11-15 19:30:41 -0500

    refactor: styling and layout for feature pay section

--- src/components/home/FeaturePay.jsx
+++ src/components/home/FeaturePay.jsx
@@ -1,39 +1,24 @@
 import React from 'react';
-import PlayMockup from '../../assets/images/play-mockup.png';
-import PayMobileMockup from '../../assets/images/pay-mobile.png';
+import PlayMockup from '../../assets/pages/home/feature-pay-section-image.png';
+import PayMobileMockup from '../../assets/pages/home/feature-pay-section-image-mobile.png';
 import GlowButton from '../button/Glow';
 
 const FeaturePay = () => {
   return (
-    <div className="lg:mx-2 mx-3 mt-[80px] md:mt-[120px]">
-      <div className="play-section max-w-7xl mx-auto ">
-        <div className="md:grid grid-cols-2 gap-10 md:h-[620px] overflow-hidden  flex flex-col-reverse md:flex-row">
-          <div className="flex justify-center">
-            <img
-              src={PlayMockup}
-              className=" hidden md:block md:w-full"
-              alt=""
-            />
-            <img
-              src={PayMobileMockup}
-              className="md:hidden"
-              alt=""
-            />
+    <div className="flex flex-col-reverse tablet:flex-row-reverse md:flex-row items-center justify-between gap-10">
+      <div className='flex-1'>
+        <img src={PlayMockup} className="hidden md:block w-full object-contain" alt="" />
+        <img src={PayMobileMockup} className="md:hidden" alt="" />
+      </div>
+      <div className="flex flex-1 items-center">
+        <div className="space-y-4">
+          <div className="text-[22px] md:text-4xl font-bold">Pay Friends</div>
+          <div className="primary-text w-full md:max-w-[525px]">
+            Split bills and share costs effortlessly. Mun-e lets you settle up instantly, keeping
+            group expenses simple and stress-free.
           </div>
-          <div className="md:h-[620px]  flex justify-start items-center">
-            <div className="space-y-4">
-              <div className=" text-[22px] md:text-4xl font-bold">
-                Pay Friends
-              </div>
-              <div className="primary-text w-full md:w-[525px]">
-                Split bills and share costs effortlessly.
-                Mun-e lets you settle up instantly, keeping
-                group expenses simple and stress-free.
-              </div>
-              <div className="hidden md:block">
-                <GlowButton color='green'>Get Mun-e</GlowButton>
-              </div>
-            </div>
+          <div className="hidden md:block">
+            <GlowButton color="green">Get Mun-e</GlowButton>
           </div>
         </div>
       </div>
--- src/components/home/Layout.tsx
+++ src/components/home/Layout.tsx
@@ -5,7 +5,7 @@ import Footer from '../Footer'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Layout({ children }: any) {
   return (
-    <div>
+    <div className="max-w-[1280px] mx-auto">
         <Header />
           {children}
         <Footer /> 
