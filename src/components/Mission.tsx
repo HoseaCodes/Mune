06cce184a25c1df5d9db4c6e735ccb769c168340
Riviere Fougy <rivierefougy@gmail.com>
2024-11-15 20:19:38 -0500

    refactor(Mission): responvice stying and layout

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -5,9 +5,9 @@ import familySelfie from '../assets/pages/about/family-selfie.webp';
 
 const MissionSection: React.FC = () => {
   return (
-    <div className="relative py-20 px-8">
-      <div className="absolute top-0 left-0 flex flex-col gap-2 bg-[#F2FDF9F2] border-2 border-[#D9E9E2] rounded-[48px] max-w-fit py-8 px-6">
-        <h2 className="font-bold text-4xl text-[#182E26]">
+    <div className="relative py-10 laptop:py-20 px-6 laptop:px-8 flex flex-col items-center border-2 laptop:border-none rounded-[32px] border-[#D9E9E2] laptop:bg-none">
+      <div className="laptop:absolute laptop:top-0 laptop:left-0 flex flex-col gap-2 items-center laptop:items-start laptop:bg-[#F2FDF9F2] laptop:border-2 border-[#D9E9E2] rounded-[48px] max-w-fit py-8 px-6">
+        <h2 className="font-bold text-4xl text-center text-[#182E26]">
           How we give back to the community:
         </h2>
         <h2 className="font-bold text-4xl text-[#19A530]">
@@ -16,8 +16,8 @@ const MissionSection: React.FC = () => {
       </div>
       <img
         src={familySelfie}
-        className="border-2 border-[#D9E9E2] rounded-[48px]"
-      />
+        className="border-2 border-[#D9E9E2] rounded-[48px] aspect-[16/10] laptop:aspect-[16/9] object-cover w-full"
+        />
     </div>
   );
 };
